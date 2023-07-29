a63d8b8a35f4177c6d5037a4f7757034bac5f490
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-07-29 17:16:32 +0530

    update:05

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { videos } from "../api/videos/videos";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { WatchLaterContext } from "../context/WatchLaterContext";
 export const Explore = () => {
+  const [search, setSearch] = useState("");
+
   const navigate = useNavigate();
   const { watchLaterItems, addToWatchLater, removeFromWatchLater } =
     useContext(WatchLaterContext);
@@ -10,10 +12,32 @@ export const Explore = () => {
   const checkId = (selectedId) => {
     return watchLaterItems.find((video) => video._id === selectedId);
   };
+
+  let displayVideos = videos;
+
+  displayVideos = displayVideos.filter((video) =>
+    video.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="px-4 mt-8">
+      <p className="text-2xl font-bold px-6">Explore</p>
+      <div className="px-36 mt-4">
+        <input
+          type="text"
+          name="search"
+          placeholder="Search video by title"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="border w-full p-2 text-sm"
+        />
+      </div>
+
       <div className="flex flex-wrap ">
-        {videos.map((video) => (
+        {displayVideos.length < 1 && (
+          <p className="font-bold mt-16">No videos found !!</p>
+        )}
+        {displayVideos.map((video) => (
           <div
             key={video._id}
             className="mb-12 p-6 cursor-pointer w-60 relative"
